0495598056769cd99b0a41ff139d4ab4e85bf70c
Noah Robitshek <64294283+NoahRobit72@users.noreply.github.com>
2023-04-16 22:26:12 -0400

    add file

---
+++ research-app/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import Footer from './Footer';
+import SearchBar from './SearchBar';
+
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+      <SearchBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ research-app/src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="Footer">
+      <p>&copy; 2023 My Website</p>
+    </footer>
+  );
+}
+
+export default Footer;
